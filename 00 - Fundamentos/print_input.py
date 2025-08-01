8fa86251b79ece029a8ef02caea683e7172f1e95
Liziane Teixeira Vaz <132149315+lizaraxamg@users.noreply.github.com>
2025-08-01 06:04:42 -0300

    Update print_input.py

--- 00 - Fundamentos/print_input.py
+++ 00 - Fundamentos/print_input.py
@@ -1,7 +1,11 @@
 nome = input("Informe o seu nome: ")
 idade = input("Informe a sua idade: ")
+escolaridade = input("Informe a sua escolaridade:")
+genero = input("Informe seu genero:")
 
 print(nome, idade)
 print(nome, idade, end="...\n")
 print(nome, idade, sep="#", end="...\n")
 print(nome, idade, sep="#")
+print(nome, idade, escolaridade)
+print(nome,idade, genero)
